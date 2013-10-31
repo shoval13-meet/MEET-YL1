fb13a13d430de5a16e7ce30c45b09718e72e0488
Shoval Peleg <shoval13@meet.mit.edu>
2013-10-31 20:14:15 +0200

    finished lab 1

---
+++ Palindrome.py~
@@ -0,0 +1,18 @@
+#Exercise B: isPalindrome
+
+def Palindrome()
+
+	a=raw_input("write down a word")
+	b=a
+	a=a[::-1]
+	
+	if b=a:
+		print "true"
+	elif 	print "false" 
+
+if __name__=="__main__":
+		Palindrome()
+	
+
+
+
---
+++ divisors.py
@@ -0,0 +1,12 @@
+#Exercise A: divisors
+def divisors(n):
+       for some_numbers in range (1,n):
+            if n%some_numbers==0:
+		     print some_numbers 
+if __name__=="__main__":
+	  divisors(int(raw_input("enter number")))
+
+
+
+ 
+
